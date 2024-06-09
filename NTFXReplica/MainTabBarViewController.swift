43993ed9c6ce52932d6840e20b8a0c8eccce4e54
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-09 09:54:41 +0200

    4 tabVCs were added to the mainTabBarVC.

--- NTFXReplica/MainTabBarViewController.swift
+++ NTFXReplica/MainTabBarViewController.swift
@@ -12,6 +12,12 @@ class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemYellow
+        
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: ComingSoonViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadsViewController())
+        
     }
 
 
