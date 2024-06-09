d366b6daf3ed68d9cf0deb828ddf89f48490e5a2
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-09 10:33:12 +0200

    VCs colors changed to systemBackground.

--- NTFXReplica/Controllers/ComingSoonViewController.swift
+++ NTFXReplica/Controllers/ComingSoonViewController.swift
@@ -11,8 +11,7 @@ class ComingSoonViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGreen
-
+        view.backgroundColor = .systemBackground
     }
     
 
--- NTFXReplica/Controllers/DownloadsViewController.swift
+++ NTFXReplica/Controllers/DownloadsViewController.swift
@@ -11,8 +11,7 @@ class DownloadsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
-
+        view.backgroundColor = .systemBackground
     }
 
 }
