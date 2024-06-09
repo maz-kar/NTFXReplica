da7a1d5f3d671f64447cbce1194fea5c5bf4ac8a
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-09 17:11:46 +0200

    new collectionViewCell added.

--- NTFXReplica/Views/CollectionViewTableViewCell.swift
+++ NTFXReplica/Views/CollectionViewTableViewCell.swift
@@ -8,16 +8,48 @@
 import UIKit
 
 class CollectionViewTableViewCell: UITableViewCell {
-    
+
     static let identifier = "CollectionViewTableViewCell"
+    
+    private var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 140, height: 200)
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        return collectionView
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.backgroundColor = .systemPink
+        contentView.addSubview(collectionView)
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
     }
     
     required init?(coder: NSCoder) {
         fatalError()
     }
+    
+}
 
+extension CollectionViewTableViewCell: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    
 }
