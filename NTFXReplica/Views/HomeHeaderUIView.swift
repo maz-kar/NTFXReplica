02aeeaef984a9de856595f8deca20913bc860c14
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-09 19:43:47 +0200

    Play and Download buttons were added.

--- NTFXReplica/Views/HomeHeaderUIView.swift
+++ NTFXReplica/Views/HomeHeaderUIView.swift
@@ -9,6 +9,26 @@ import UIKit
 
 class HomeHeaderUIView: UIView {
     
+    private let playButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Download", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     private let headerImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -27,10 +47,32 @@ class HomeHeaderUIView: UIView {
         layer.addSublayer(gradientLayer)
     }
     
+    private func applyConstraints() {
+        let playConstraints = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            playButton.widthAnchor.constraint(equalToConstant: 120)
+        ]
+        
+        let downloadConstraints = [
+            downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -60),
+            downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            downloadButton.widthAnchor.constraint(equalToConstant: 120)
+        ]
+        
+        NSLayoutConstraint.activate(playConstraints)
+        NSLayoutConstraint.activate(downloadConstraints)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(headerImageView)
         addGradient()
+        addSubview(playButton)
+        addSubview(downloadButton)
+        applyConstraints()
+        
+        
     }
     
     required init?(coder: NSCoder) {
