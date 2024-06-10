d758965e5a901787030acf9122a40c1f5320ce05
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-10 16:14:36 +0200

    Redundant TV model was deleted. Movie model was renamed to Title.

--- NTFXReplica/Title.swift
+++ NTFXReplica/Title.swift
@@ -1,5 +1,5 @@
 //
-//  Movie.swift
+//  Title.swift
 //  NTFXReplica
 //
 //  Created by Maziar Layeghkar on 10.06.24.
@@ -7,11 +7,11 @@
 
 import Foundation
 
-struct TrendingMoviesResponse: Codable {
-    let results: [Movie]
+struct TrendingTitleResponse: Codable {
+    let results: [Title]
 }
 
-struct Movie: Codable {
+struct Title: Codable {
     let id: Int
     let media_type: String?
     let original_name: String?
