c27bac0e7a901650ac64ceedd40e7a164c2b5a9e
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-10 14:45:57 +0200

    connection with the database was established successfully.

--- NTFXReplica/Managers/APICaller.swift
+++ NTFXReplica/Managers/APICaller.swift
@@ -8,23 +8,27 @@
 import Foundation
 
 struct Constants {
-    static let API_KEY = "257007683961dbd0c49e765549682fc2"
-    static let baseURL = "https://api.themoviedb.org/"
+    static let API_KEY = "697d439ac993538da4e3e60b54e762cd"
+    static let baseURL = "https://api.themoviedb.org"
 }
 
 class APICaller {
     static let shared = APICaller()
     
-    func getTrendingMovies(completion: @escaping (String) -> ()) {
+    func getTrendingMovies(completion: @escaping (String) -> Void) {
+        
         guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else { return }
+        
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
             guard let data = data, error == nil else { return }
+            
             do {
                 let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
                 print(results)
             } catch {
                 print(error.localizedDescription)
             }
+            
         }
         task.resume()
     }
