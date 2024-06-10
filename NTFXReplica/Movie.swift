ee49156b8df2863f2dd7da84f74b9d76eb8596cd
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-10 15:04:17 +0200

    MovieModel was created and data was successfully downloaded.

---
+++ NTFXReplica/Movie.swift
@@ -0,0 +1,24 @@
+//
+//  Movie.swift
+//  NTFXReplica
+//
+//  Created by Maziar Layeghkar on 10.06.24.
+//
+
+import Foundation
+
+struct TrendingMoviesResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int
+    let media_type: String?
+    let original_name: String?
+    let original_title: String?
+    let poster_path: String?
+    let overview: String?
+    let vote_count: Int
+    let release_date: String?
+    let vote_average: Double
+}
