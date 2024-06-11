2fb72c1477a64c2a6e0dbb3592150bb1245de0bf
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-11 11:38:55 +0200

    delegate and datasource methods added to the tableView.

--- NTFXReplica/Controllers/ComingSoonViewController.swift
+++ NTFXReplica/Controllers/ComingSoonViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ComingSoonViewController: UIViewController {
-
+    
+    private let comingSoonTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -16,7 +22,29 @@ class ComingSoonViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationItem.largeTitleDisplayMode = .always
         
+        view.addSubview(comingSoonTable)
+        
+        comingSoonTable.delegate = self
+        comingSoonTable.dataSource = self
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        comingSoonTable.frame = view.bounds
+    }
+
+}
+
+extension ComingSoonViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
+    }
 
 }
