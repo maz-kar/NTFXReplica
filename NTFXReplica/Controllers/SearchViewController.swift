e14e994a7c668c6f0772d27fbb95106f7f98c132
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-11 13:18:04 +0200

    initial UI of searchVC was completed.

--- NTFXReplica/Controllers/SearchViewController.swift
+++ NTFXReplica/Controllers/SearchViewController.swift
@@ -8,13 +8,43 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    private let searchTable: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier) //TitleTableViewCell is a generic TVC
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        title = "Search"
+        title = "Top Searches"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(searchTable)
+        
+        searchTable.delegate = self
+        searchTable.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchTable.frame = view.bounds
     }
 
+}
+
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
+    
+    
 }
