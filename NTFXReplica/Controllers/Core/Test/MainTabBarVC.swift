de72ca3f144d57a0e6cdf4d0190dc7f2e4723c4c
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-19 22:52:01 +0200

    New VCs added to TabBar as UINavigationController.

--- NTFXReplica/Controllers/Core/Test/MainTabBarVC.swift
+++ NTFXReplica/Controllers/Core/Test/MainTabBarVC.swift
@@ -13,7 +13,24 @@ class MainTabBarVC: UITabBarController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         
+        let vc1 = UINavigationController(rootViewController: HomeVC())
+        let vc2 = UINavigationController(rootViewController: UpcomingVC())
+        let vc3 = UINavigationController(rootViewController: SearchVC())
+        let vc4 = UINavigationController(rootViewController: DownloadVC())
         
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        vc1.title = "Home"
+        vc2.title = "Upcomings"
+        vc3.title = "Top Searches"
+        vc4.title = "Downloads"
+        
+        tabBar.tintColor = .label
+        
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
     }
 
 }
